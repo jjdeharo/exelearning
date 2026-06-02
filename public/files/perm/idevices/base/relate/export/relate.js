f092412ef4b4a1809d254eb74f89281874b90b05
FranM <franmate@gmail.com>
2026-06-02 13:58:50 +0200

    fix: update LaTeX rendering on 'Relate' interactive activity restart (#1872)
    * fix: update LaTeX rendering on 'Relate' interactive activity restart
    
    * fix: add null check for game container node to prevent errors
    
    * style: format code block according to biome rules

--- public/files/perm/idevices/base/relate/export/relate.js
+++ public/files/perm/idevices/base/relate/export/relate.js
@@ -641,7 +641,14 @@ var $eXeRelaciona = {
         $eXeRelaciona.showScoreGame(instance);
         mOptions.gameStarted = true;
         mOptions.gameOver = false;
-
+        const nodeGame = document.querySelector('#rlcContainerGame-' + instance);
+        if (nodeGame) {
+            if ($exeDevices.iDevice.gamification.math.hasLatex(nodeGame.innerHTML)) {
+                $exeDevices.iDevice.gamification.math.updateLatex(
+                    '#rlcContainerGame-' + instance,
+                );
+            }
+        }
         $('#rlcMessage-' + instance).hide();
     },
 
